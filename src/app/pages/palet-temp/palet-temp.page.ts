0ce9eb34fe58bd6aa3091d7efb173e1af41733ff
joseeson1261 <joseson1261@gmail.com>
2022-12-15 16:16:33 -0700

    Antonio

--- src/app/pages/palet-temp/palet-temp.page.ts
+++ src/app/pages/palet-temp/palet-temp.page.ts
@@ -18,26 +18,7 @@ export class PaletTempPage implements OnInit {
   }
 
   ngOnInit() {
-  fetch('./assets/data/temporal.json').then( res => res.json())//Cambiar esto por lo de la base de datos 
-  .then(json => {
-    this.pallets = json;
-    console.log(this.pallets)
-    
-    this.tabla.paletas = 'Pallets: ' + this.pallets.length
-    this.totalkgs = 0;
-    this.totalcajas = 0;
-    var v: any;
-    for (v=0; v < this.pallets.length; v++){
-      this.totalkgs += this.pallets[v].kgs
-      this.totalcajas += this.pallets[v].caj
-    }
-    this.tabla.cajas = 'Cajas: ' + this.totalcajas
-    this.tabla.kgs = 'KGS: ' + this.totalkgs
-
-    console.log(this.tabla.paletas)
-    console.log(this.tabla.cajas)
-    console.log(this.tabla.kgs)
-  });
+  
   }
 
 }

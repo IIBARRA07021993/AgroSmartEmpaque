92ec851365a47d472d350ee7bd3c88333e47ea1b
IIBARRA <ismael.ibarra@inventum.com.mx>
2022-12-20 10:49:40 -0700

    cambios en la interfas

---
+++ src/app/models/color.model.ts
@@ -0,0 +1,13 @@
+export class EyeColor {
+  c_codigo_col: string;
+  v_nombre_col: string;
+  c_codigoalt_col: string;
+  c_colorcomer_col: string;
+
+  constructor() {
+    this.c_codigo_col = '';
+    this.v_nombre_col = '';
+    this.c_codigoalt_col = '';
+    this.c_colorcomer_col = '';
+  }
+}
--- src/app/pages/paletvirtual/paletvirtual.page.ts
+++ src/app/pages/paletvirtual/paletvirtual.page.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { IonInput } from '@ionic/angular';
-import { Bandas } from 'src/app/interfaces/interfaces';
+import { Bandas, Color, Etiqueta} from 'src/app/interfaces/interfaces';
+import { EyeProducto } from 'src/app/models/producto.model';
+import { GetdatosService } from 'src/app/services/getdatos.service';
+import { UtilService } from 'src/app/services/util.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-paletvirtual',
@@ -11,20 +15,132 @@ import { Bandas } from 'src/app/interfaces/interfaces';
 export class PaletvirtualPage implements OnInit {
   @ViewChild('codpal', { static: false }) codpal!: IonInput;
 
-  
   banda: Bandas;
-  c_codigo_pal: string = '';
-  
-  constructor(private route: ActivatedRoute) {}
+
+  producto: EyeProducto = new EyeProducto()
+  etiqueta: Etiqueta;
+  color: Color;
+
+  constructor(
+    private route: ActivatedRoute,
+    private getdatoserv: GetdatosService,
+    private ultilService: UtilService
+  ) {}
 
   ngOnInit() {
     this.route.queryParams.subscribe((Params: Bandas) => {
       this.banda = Params;
     });
+
+
+    
   }
 
+  enterkey() {}
+  fn_scanner() {}
 
-  enterkey(){}
+  buscarProducto() {
+    return new Promise((resolve) => {
+      var json = {};
 
-  fn_scanner(){}
+      console.log(JSON.stringify(json));
+
+      this.getdatoserv
+        .sp_AppGetDatos(
+          '/GetDatos?as_empresa=' +
+            environment.codempresa +
+            '&as_operation=14&as_json=' +
+            JSON.stringify(json)
+        )
+        .subscribe(
+          (resp: string) => {
+            console.log(resp);
+            this.producto = JSON.parse(resp);
+            console.log(this.producto);
+            resolve(true);
+          },
+          (error) => {
+            console.error(JSON.stringify(error));
+            this.ultilService.presentToast(
+              'Error!',
+              'Ocurrio un error Interno.',
+              500,
+              'warning-outline',
+              'danger'
+            );
+            resolve(false);
+          }
+        );
+    });
+  }
+
+  buscarEtiqueta() {
+    return new Promise((resolve) => {
+      var json = {};
+
+      console.log(JSON.stringify(json));
+
+      this.getdatoserv
+        .sp_AppGetDatos(
+          '/GetDatos?as_empresa=' +
+            environment.codempresa +
+            '&as_operation=15&as_json=' +
+            JSON.stringify(json)
+        )
+        .subscribe(
+          (resp: string) => {
+            console.log(resp);
+            this.etiqueta = JSON.parse(resp);
+            console.log(this.etiqueta);
+            resolve(true);
+          },
+          (error) => {
+            console.error(JSON.stringify(error));
+            this.ultilService.presentToast(
+              'Error!',
+              'Ocurrio un error Interno.',
+              500,
+              'warning-outline',
+              'danger'
+            );
+            resolve(false);
+          }
+        );
+    });
+  }
+
+  buscarColor() {
+    return new Promise((resolve) => {
+      var json = {};
+
+      console.log(JSON.stringify(json));
+
+      this.getdatoserv
+        .sp_AppGetDatos(
+          '/GetDatos?as_empresa=' +
+            environment.codempresa +
+            '&as_operation=16&as_json=' +
+            JSON.stringify(json)
+        )
+        .subscribe(
+          (resp: string) => {
+            console.log(resp);
+            this.color = JSON.parse(resp);
+            console.log(this.color);
+            resolve(true);
+          },
+          (error) => {
+            console.error(JSON.stringify(error));
+            this.ultilService.presentToast(
+              'Error!',
+              'Ocurrio un error Interno.',
+              500,
+              'warning-outline',
+              'danger'
+            );
+            resolve(false);
+          }
+        );
+    });
+  }
 }

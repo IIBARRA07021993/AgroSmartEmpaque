a97b52319495ca30a9ce8aa42ffac6fc17fe17e6
IIBARRA <ismael.ibarra@inventum.com.mx>
2022-12-30 17:00:09 -0700

    cambios en los pedidos

--- src/app/models/presentacion.model.ts
+++ src/app/models/presentacion.model.ts
@@ -1,13 +1,18 @@
 export class EyePresentacion {
     c_codigo_pro: string;
+    v_nombre_pro: string;
     c_codigo_eti: string;
+    v_nombre_eti: string;
     c_codigo_col: string;
-   
+    v_nombre_col: string;
   
     constructor() {
       this.c_codigo_pro = '';
+      this.v_nombre_pro = '';
       this.c_codigo_eti = '';
+      this.v_nombre_eti = '';
       this.c_codigo_col = '';
+      this.v_nombre_col = '';
 
     }
   }
